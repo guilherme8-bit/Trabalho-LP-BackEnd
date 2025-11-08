f60604bf0192ab3a3761b76e2aa924ab55421ca0
André63359 <salomaoandre383@gmail.com>
2025-11-08 16:53:47 -0300

    telas de filme e serie

---
+++ view/filme.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filme</title>
+    <link rel="stylesheet" href="../estilizacao/filme.css">
+</head>
+<body>
+    <nav>
+        <img class="casa" src="/img/house.png" alt="">
+        <h2><a href="home.html">Home</a></h2>
+    </nav>
+        <section class="titulo">
+            <img class="filme" src="/img/camera.png" alt="">
+                <h3>Filmes</h3>
+        </section>
+            <section class="bloco-principal">
+                <h3>Ação</h3>
+                <section class="flex-filmes">
+                    <section class="card"><img src="../img/filmes/batman.jpg"></section>
+                    <section class="card"><img src="../img/filmes/aquaman.jpg"></section>
+                    <section class="card"><img src="../img/filmes/jw.jpg"></section>
+                    <section class="card"><img src="../img/filmes/mi.jpg"></section>
+                    <section class="card"><img src="../img/filmes/John_Wick_2.jpg"></section>
+                    <section class="card"><img src="../img/filmes/dw.webp"></section>
+                    <section class="card"><img src="../img/filmes/logan.webp"></section>
+                    <section class="card"><img src="../img/filmes/transformers.jpe"></section>
+                    <section class="card"><img src="../img/filmes/avatar.jpg"></section>
+                    <section class="card"><img src="../img/filmes/tropa-de-elite.webp"></section>
+                </section>
+              
+                <h3>Aventura</h3>
+                <section class="flex-filmes">
+                    <section class="card"><img src="../img/filmes/O_Rei_Leão_2019.jpg"></section>
+                    <section class="card"><img src="../img/filmes/hobbit.jpg"></section>
+                    <section class="card"><img src="../img/filmes/tintin.jpg"></section>
+                    <section class="card"><img src="../img/filmes/pj.jpg"></section>
+                    <section class="card"><img src="../img/filmes/Piratas do Caribe 5.jpg"></section>
+                    <section class="card"><img src="../img/filmes/jumanji.webp"></section>
+                    <section class="card"><img src="../img/filmes/dragao.jpg"></section>
+                    <section class="card"><img src="../img/filmes/ps.jpeg"></section>
+                    <section class="card"><img src="../img/filmes/Alice-In-Wonderland-Theatrical-Poster.jpg"></section>
+                    <section class="card"><img src="../img/filmes/moana.jpg"></section>
+                </section>
+            </section>
+             <footer class="main-footer">
+        <section class="footer-content">
+            <section class="footer-section about">
+                <h3>MOVIEFLIX</h3>
+                <p>O melhor lugar para maratonar filmes e séries. Cancelamento fácil a qualquer momento.</p>
+            </section>
+
+            <section class="footer-section links">
+                <h3>Links Rápidos</h3>
+                <ul>
+                    <li><a href="#">Sobre Nós</a></li>
+                    <li><a href="#">Perguntas Frequentes</a></li>
+                    <li><a href="#">Termos de Uso</a></li>
+                    <li><a href="#">Política de Privacidade</a></li>
+                </ul>
+            </section>
+
+            <section class="footer-section social">
+                <h3>Siga-nos</h3>
+                <section class="social-icons">
+                    <a href="#"><img src="../img/instagram.png" alt="instagrma"></a>
+                    <a href="#"><img src="../img/facebook.png" alt="facebook"></a>
+                    <a href="#"><img src="../img/ex-twitter.png" alt="twitter"></a>
+                </section>
+            </section>
+        </section>
+
+        <section class="footer-bottom">
+            &copy; <span id="current-year"></span> Movieflix. Todos os direitos reservados.
+        </section>
+    </footer>
+            
+</body>
+</html>
---
+++ view/serie.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serie</title>
+    <link rel="stylesheet" href="../estilizacao/serie.css">
+</head>
+<body>
+    <nav>
+        <img class="casa" src="/img/house.png" alt="">
+        <h2><a href="home.html">Home</a></h2>
+    </nav>
+        <section class="titulo">
+            <img class="filme" src="/img/tv_show.png" alt="">
+                <h3>Séries</h3>
+        </section>
+            <section class="bloco-principal">
+                <h3>Ação</h3>
+                <section class="flex-filmes">
+                    <section class="card"><img src="../img/series/pacificador.jpg"></section>
+                    <section class="card"><img src="../img/series/lacasa.webp"></section>
+                    <section class="card"><img src="../img/series/falcao.webp"></section>
+                    <section class="card"><img src="../img/series/flash.jpg"></section>
+                    <section class="card"><img src="../img/series/alien.jpg"></section>
+                    <section class="card"><img src="../img/series/peaky blinders.jpg"></section>
+                    <section class="card"><img src="../img/series/got.jpg"></section>
+                    <section class="card"><img src="../img/series/ck.jpg"></section>
+                    <section class="card"><img src="../img/series/arrow.jpg"></section>
+                    <section class="card"><img src="../img/series/the boys.jpg"></section>
+                </section>
+              
+                <h3>Aventura</h3>
+                <section class="flex-filmes">
+                    <section class="card"><img src="../img/series/op.png"></section>
+                    <section class="card"><img src="../img/series/The_Mandalorian_season_2_poster.jpg"></section>
+                    <section class="card"><img src="../img/series/vk.png"></section>
+                    <section class="card"><img src="../img/series/doctor_who.jpg"></section>
+                    <section class="card"><img src="../img/series/rm.jpg"></section>
+                    <section class="card"><img src="../img/series/gf.webp"></section>
+                    <section class="card"><img src="../img/series/tw.jpg"></section>
+                    <section class="card"><img src="../img/series/sn.webp"></section>
+                    <section class="card"><img src="../img/series/fallout.jpg"></section>
+                    <section class="card"><img src="../img/series/pco.webp"></section>
+                </section>
+            </section>
+            <footer class="main-footer">
+        <section class="footer-content">
+            <section class="footer-section about">
+                <h3>MOVIEFLIX</h3>
+                <p>O melhor lugar para maratonar filmes e séries. Cancelamento fácil a qualquer momento.</p>
+            </section>
+
+            <section class="footer-section links">
+                <h3>Links Rápidos</h3>
+                <ul>
+                    <li><a href="#">Sobre Nós</a></li>
+                    <li><a href="#">Perguntas Frequentes</a></li>
+                    <li><a href="#">Termos de Uso</a></li>
+                    <li><a href="#">Política de Privacidade</a></li>
+                </ul>
+            </section>
+
+            <section class="footer-section social">
+                <h3>Siga-nos</h3>
+                <section class="social-icons">
+                    <a href="#"><img src="../img/instagram.png" alt="instagrma"></a>
+                    <a href="#"><img src="../img/facebook.png" alt="facebook"></a>
+                    <a href="#"><img src="../img/ex-twitter.png" alt="twitter"></a>
+                </section>
+            </section>
+        </section>
+
+        <section class="footer-bottom">
+            &copy; <span id="current-year"></span> Movieflix. Todos os direitos reservados.
+        </section>
+    </footer>
+</body>
+</html>
